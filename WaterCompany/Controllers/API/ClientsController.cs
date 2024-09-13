ec72e566017c91ea740d29c6011ec9cc7d385429
hugosousa42 <hugosousa42@proton.me>
2024-09-13 09:01:45 +0000

    API clients controller

---
+++ WaterCompany/Controllers/API/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using WaterCompany.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WaterCompany.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientsController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetClients()
+        {
+            return Ok(_clientRepository.GetAll());
+        }
+    }
+}
